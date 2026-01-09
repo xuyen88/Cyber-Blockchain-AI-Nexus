81dff0882a44746cc8ed0ed90c0b066d4fed8f2c
Xuyen_Blockchain_Dev <35366890+xuyen88@users.noreply.github.com>
2026-01-09 14:19:38 +0700

    Add files via upload

---
+++ Phase-1-Foundations/day_06_challenge.py
@@ -0,0 +1,12 @@
+# 1. Danh sách số ban đầu
+numbers = [1, 5, 8, 12, 15, 22, 7, 30]
+
+print("Các số lớn hơn 10 trong danh sách là:")
+
+# 2. Vòng lặp duyệt qua từng số
+for num in numbers:
+    # 3. Kiểm tra điều kiện
+    if num > 10:
+        print(f"👉 Số: {num}")
+
+print("--- Hoàn thành thử thách! ---")
